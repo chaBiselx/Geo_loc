5e101aa7ed3b63bb8799f00c2061d0599f96050f
Charles BISELX <cha.biselx@gmail.com>
2017-11-24 11:14:57 +0100

    refactoring

--- controllers/maps.js
+++ controllers/maps.js
@@ -17,7 +17,12 @@ exports.getMaps = (req, res) => {
 exports.postActualise = (req, res) => {
   console.log("actualise db");
 
+  httprequest(res, res, stanlib);
 
+  res.redirect('/maps');
+}
+
+function httprequest(res, res, url){
   http.get(stanlib, (resp) => {
   let data = '';
 
@@ -28,7 +33,26 @@ exports.postActualise = (req, res) => {
 
   // The whole response has been received. Print out the result.
   resp.on('end', () => {
-    console.log(JSON.parse(data)); //TODO COnvert in db 
+    console.log(data); //TODO COnvert in db
+  });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+}
+
+function httpsrequest(res, res, url){
+  https.get(stanlib, (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(data); //TODO COnvert in db
   });
 
   }).on("error", (err) => {
